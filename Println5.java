eb18763c97eaf1aea2a59e691542a75ab560b742
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 15:48:11 +0530

    Create Println5.java

---
+++ Println5.java
@@ -0,0 +1,12 @@
+package Practicecodes.Java.Assignment1java;
+
+public class Println5 {
+    public static void main(String[] args) {
+        System.out.println("       '__'");
+        System.out.println("       (oo)");
+        System.out.println("  /========//");
+        System.out.println(" / || @@ ||");
+        System.out.println("*  ||----||");
+        System.out.println("   VV    VV");
+    }
+}
