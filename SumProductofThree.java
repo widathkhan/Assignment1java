434ed4a189bf208b0f63001273aacb77c6df131d
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 15:52:28 +0530

    Create SumProductofThree.java

---
+++ SumProductofThree.java
@@ -0,0 +1,10 @@
+package Practicecodes.Java.Assignment1java;
+
+public class SumProductofThree {
+    public static void main(String[] args) {
+        int number1=98,number2=5,number3=77;
+        int sum=number1+number2+number3, product=number1*number2*number3;
+        System.out.println("The sum of the three numbers is "+sum);
+        System.out.println("The product of the three numbers is "+product);
+    }
+}
