327ab95a40b5135814f47f1c9e64695d5679801d
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 15:46:57 +0530

    Create Println2.java

---
+++ Println2.java
@@ -0,0 +1,11 @@
+package Practicecodes.Java.Assignment1java;
+
+public class Println2 {
+    public static void main(String[] args) {
+        System.out.println("* * * * *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("* * * * *");
+    }
+}
