aef566de5f0d318e68f8f43a39fabc429bebd711
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 15:47:44 +0530

    Create Println4.java

---
+++ Println4.java
@@ -0,0 +1,12 @@
+package Practicecodes.Java.Assignment1java;
+
+public class Println4 {
+    public static void main(String[] args) {
+        System.out.println("    *");
+        System.out.println("* *   * *");
+        System.out.println("   * *");
+        System.out.println("  *   *");
+        System.out.println("*       *");
+    }
+    
+}
