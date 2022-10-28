0bc69df48ad3f280ca19550293e96fa8d8a8ada6
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 15:45:05 +0530

    Create Explicitmultiplier.java

---
+++ Explicitmultiplier.java
@@ -0,0 +1,11 @@
+package Practicecodes.Java.Assignment1java;
+
+public class Explicitmultiplier {
+    public static void main(String[] args) {
+        int number1=98,number2=5;
+        int product1=31*number1;
+        int product2=17*number2;
+        System.out.println("31 times of number1 is "+product1);
+        System.out.println("17 times of number2 is "+product2);
+    }
+}Explicitmultiplier.java
