8e77c7c3c5417feaa0550b6063a04c32bf7b8c1e
widathkhan <114463526+widathkhan@users.noreply.github.com>
2022-10-28 15:46:32 +0530

    Create Println1.java

---
+++ Println1.java
@@ -0,0 +1,12 @@
+package Practicecodes.Java.Assignment1java;
+
+public class Println1 {
+    public static void main(String[] args) {
+        //Using a single sysout to print each line
+        System.out.println("* * * * *");
+        System.out.println(" * * * * *");
+        System.out.println("* * * * *");
+        System.out.println(" * * * * *");
+        System.out.println("* * * * *");
+    }
+}
